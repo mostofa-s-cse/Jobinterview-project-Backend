59f363a0def55722228eda6a5097876eb81d6556
mostofa-s-cse <mostofa.s.cse@gmail.com>
2023-10-09 01:29:05 +0600

    register login todo

---
+++ app/Models/EmployeeActivities.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EmployeeActivities extends Model
+{
+    use HasFactory;
+}
--- database/migrations/2023_10_08_182023_create_employee_activities_table.php
+++ database/migrations/2023_10_08_182023_create_employee_activities_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('employee_activities', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role',["Owner","Employee"])->default("Employee");
-            $table->rememberToken();
+            $table->string('check_in');
+            $table->string('employee_id');
+            $table->string('check_out');
+            $table->string('office_hour');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('employee_activities');
     }
 };
